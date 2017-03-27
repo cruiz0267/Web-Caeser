707101a377eb0f43e76883835f2141aaadda2fe6
CHRIS <cruiz0267@gmail.com>
2017-03-26 20:03:33 -0500

    added basic site design

--- main.py
+++ main.py
@@ -21,7 +21,10 @@ class MainHandler(webapp2.RequestHandler):
     def get(self):
         text = "hello world"
         encrypted_message = caeser.encrypt(text,13)
-        self.response.write(encrypted_message)
+        textarea = "<textarea>" + encrypted_message + "</textarea>"
+        submit = "<input type='submit'/>"
+        form = "<form>" + textarea + "<br>" + submit + "</form>"
+        self.response.write(form)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
